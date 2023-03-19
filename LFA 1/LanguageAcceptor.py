eb61c3a81252e083499f8d67d41e16a6f353ac11
Raduc2003 <72871085+Raduc2003@users.noreply.github.com>
2023-03-20 01:01:50 +0200

    Update LanguageAcceptor.py

--- LFA 1/LanguageAcceptor.py
+++ LFA 1/LanguageAcceptor.py
@@ -1,48 +1,53 @@
-f= open("LFA 1\input.txt")
-stari={}
-stari_finale=[]
+f = open("LFA 1/input.txt")
+stari = {}
+stari_finale = []
 for line in f:
-    line=line.split(" ")
-    if len(line)==1 or(line[1][0]=='q' and line[1][1].isdigit()) :
+    line = line.split(" ")
+    if len(line) == 1 or (line[1][0] == 'q' and line[1][1:].isdigit()):
         stari_finale=line
     else:
         if line[0] not in stari.keys():
-            
-            stari.setdefault(line[0],{})
-            
-            if line[1] not in stari[line[0]].keys():
-                stari[line[0]][line[1]]=[]
-                stari[line[0]][line[1]].append(line[2].strip())
-            else:
-                stari[line[0]][line[1]].append(line[2].strip())
+            stari.setdefault(line[0], {})
+        if line[1] not in stari[line[0]].keys():
+            stari[line[0]][line[1]] = []
+            stari[line[0]][line[1]].append(line[2].strip())
         else:
-            if line[1] not in stari[line[0]].keys():
-                stari[line[0]][line[1]]=[]
-                stari[line[0]][line[1]].append(line[2].strip())
-            else:
-                stari[line[0]][line[1]].append(line[2].strip())
-# print(stari)
-# print(stari['q0']['1'])
-def tranzitie(stare,litera):
-    try:
-        noua_stare=stari[stare][litera]
-        return noua_stare[0]
-    except:
-        return -1
+            stari[line[0]][line[1]].append(line[2].strip())
+print(stari)
 
+def tranzitie(stare, litera):
+    try:
+        noile_stari = stari[stare][litera]
+        return noile_stari
+    except:
+        return []
+
+def inchidere(stari):
+    inchidere = set(stari)
+    stack = list(stari)
+    while stack:
+        stare = stack.pop()
+        for urmatoarea_stare in tranzitie(stare, 'eps'):
+            if urmatoarea_stare not in inchidere:
+                inchidere.add(urmatoarea_stare)
+                stack.append(urmatoarea_stare)
+    return inchidere
 # acceptor
-tranzitie('q0','0')
 print(stari_finale)
-cuv=input()
-drum=[]
-stare='q0'
+cuv = input()
+drum = []
+stari_curente = inchidere({'q0'})
 for litera in cuv:
-    drum.append(stare)
-    stare=tranzitie(stare,litera)
-    if stare==-1:
+    noile_stari = set()
+    for stare in stari_curente:
+        noile_stari = set(inchidere(tranzitie(stare, litera)))
+    stari_curente = noile_stari
+    if not stari_curente:
         print("neacceptat")
         break;
-if stare in stari_finale:
+    drum.extend(stari_curente) # append the current states to the drum list
+if set(stari_curente) & set(stari_finale):
     print("acceptat")
-    print(*drum,sep='->')
-
+    print("->".join(drum)) # join the elements of the drum list with "->" separator
+else:
+    print("neacceptat")
